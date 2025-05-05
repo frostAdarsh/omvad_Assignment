2e9aa281d3e5da0304e53ce0044a1b11c685c117
frostAdarsh <adarshmani017@gmail.com>
2025-05-05 23:11:42 +0530

    basic backend setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export async function   signup(req,res){
+    res.send('User signed up successfully!');
+}
+
+
+export async function login(req,res){
+    res.send('User logged in successfully!');
+}
+
+export async function logout(req,res){
+    res.send('User logged out successfully!');
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Auth route is working!');
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
