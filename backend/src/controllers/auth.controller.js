aedbc7cfa7b4f6457699f0758b5936cff0e23d7a
frostAdarsh <adarshmani017@gmail.com>
2025-05-06 13:08:43 +0530

    auth backend done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,12 +1,84 @@
-export async function   signup(req,res){
-    res.send('User signed up successfully!');
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required!" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+
+    const newUser = await User.create({
+      email,
+      password,
+    });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, message: newUser });
+  } catch (error) {
+    console.log("Error in signup controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
 
-export async function login(req,res){
-    res.send('User logged in successfully!');
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required!" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials!" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ success: true, message: user });
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export async function logout(req,res){
-    res.send('User logged out successfully!');
+export async function logout(req, res) {
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully!" });
 }
---
+++ backend/src/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+//Pre Hook Middleware to hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Method to compare password
+UserSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
